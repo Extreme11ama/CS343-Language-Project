e45826d14259dfb0213170699d40f6d792797586
Extreme11ama <wasifram@gmail.com>
2024-05-06 20:52:14 -0700

    ex update

--- BattleshipGame.rb
+++ BattleshipGame.rb
@@ -1,1 +1,20 @@
-Enter your own code here
+class Battlefield
+    attr_accessor :grid, :player1_ships, :player2_ships
+  
+    SHIP_SYMBOL = 'S'
+    HIT_SYMBOL = 'X'
+    EMPTY_SYMBOL = '-'
+
+    def initialize
+        @grid = Array.new(10) { Array.new(10, EMPTY_SYMBOL) }
+        @player1_ships = [[1,1], [2,3], [5,6]]
+        @player2_ships = [[3,4], [6,5], [7,7]]
+    end
+
+    def display_grid
+        @grid.each { |row| puts row.join(' ') }
+    end
+
+
+
+end 
